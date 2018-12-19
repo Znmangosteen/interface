b0d7d910f0b7abc6de465599822da727b8007f37
Znmangosteen <Znmangosteen@hotmail.com>
2018-12-19 20:51:05 +0800

    标签差变色

--- js/action.js
+++ js/action.js
@@ -1,73 +1,66 @@
 window.onload = function () {
     document.getElementById("commit_Edit").style.display = "none";
     document.getElementById("all_label").style.display = "none";
-
+    var labels_obj = generate_labels();
+    for (var label in labels_obj) {
+        document.getElementById("all_label").appendChild(labels_obj[label]);
+    }
 
 
 };
 
 function showFullLabel() {
     document.getElementById("label").classList.replace("col-md-1", "col-md-4");
-    document.getElementById("Edit").style.display="none";
+    document.getElementById("Edit").style.display = "none";
     document.getElementById("commit_Edit").style.display = "inline";
     document.getElementById("all_label").style.display = "inline";
+
+
 }
 
 function showSelectedLabel() {
-    document.getElementById("label").classList.replace("col-md-4","col-md-1");
+    document.getElementById("label").classList.replace("col-md-4", "col-md-1");
     document.getElementById("commit_Edit").style.display = "none";
     document.getElementById("Edit").style.display = "inline";
     document.getElementById("all_label").style.display = "none";
 
-
 }
 
-function getStyle(obj, name) {
-    if (obj.currentStyle) {
-        return obj.currentStyle[name];
+function generate_labels() {
+    var labels_title = {"grade": "面向年级", "character": "课程性质", "department": "开课院系"};
+    var labels = {"grade": "大一 大二 大三 大四", "character": "必修 选修", "department": "计算机 "};
+
+    var labels_obj = [];
+    for (var i in labels) {
+        var l_obj = document.createElement('div');
+        var ls = labels[i].split(" ");
+        var title_obj = document.createElement("h4");
+        title_obj.innerHTML = labels_title[i];
+        title_obj.classList.add("label_title");
+        l_obj.appendChild(title_obj);
+        for (var j in ls) {
+            // <button id="Edit" type="button" class="btn btn-primary " onclick="showFullLabel()">编辑</button>
+
+            var label_name = ls[j];
+
+            var single_label = document.createElement("button");
+            single_label.id = label_name;
+            single_label.classList.add("btn");
+            single_label.classList.add("btn-info");
+            single_label.classList.add("col-md-3");
+            // single_label.classList.add("label_button");
+
+            single_label.innerHTML = label_name;
+            l_obj.appendChild(single_label);
+
+            // single_label.classList.add("col-md-3");
+
+        }
+
+        labels_obj.push(l_obj);
+        var br = document.createElement("br");
+        labels_obj.push(br);
     }
-    else {
-        return getComputedStyle(obj, false)[name];
-    }
-}
 
-function startMove(obj, attr, iTarget) {
-    clearInterval(obj.timer);
-    obj.timer = setInterval(function () {
-        var cur = parseInt(getStyle(obj, attr));
-
-        var speed = (iTarget - cur) / 6;
-
-        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-
-        if (cur == iTarget) {
-            clearInterval(obj.timer);
-        }
-        else {
-            obj.style[attr] = cur + speed + 'px';
-        }
-    }, 30);
-}
-
-function changeSize(obj, iTarget1, iTarget2) {
-    clearInterval(obj.timer);
-    obj.timer = setInterval(function () {
-        var cur1 = parseInt(getStyle(obj, 'height'));
-        var cur2 = parseInt(getStyle(obj, 'width'));
-
-        var speed1 = (iTarget1 - cur1) / 6;
-        var speed2 = (iTarget2 - cur2) / 6;
-
-        speed1 = speed1 > 0 ? Math.ceil(speed1) : Math.floor(speed1);
-        speed2 = speed2 > 0 ? Math.ceil(speed2) : Math.floor(speed2);
-
-        if (cur1 === iTarget1) {
-            clearInterval(obj.timer);
-        }
-        else {
-            obj.style['height'] = cur1 + speed1 + 'px';
-            obj.style['width'] = cur2 + speed2 + 'px';
-
-        }
-    }, 30);
+    return labels_obj;
 }
