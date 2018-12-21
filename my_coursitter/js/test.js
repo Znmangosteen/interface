f5031bacb8eb6a569cc32a34b9fc2e58eb3795fc
zhaolida98 <11611803@mail.sustc.edu.cn>
2018-12-21 14:47:04 +0800

    添加完整动画以及选课逻辑

--- my_coursitter/js/test.js
+++ my_coursitter/js/test.js
@@ -12,7 +12,7 @@ function addCourse(testCourse) {
 	var oCourse;
 	var courseName = testCourse.courseName;
 	var classes = testCourse.classes;
-	res += "<div class=\"mycard\"><div class=\"card\"><div class=\"card-header\">";
+	res += "<div class=\"mycard\"><div class=\"card\"><div class=\"card-header\" onclick=removeLi(this)>";
 	res += courseName
 	res +=
 		"</div><div class=\"card-body\"><div id=\"courses\" class=\"course-selector\"><ul style=\"list-style: none; margin-left:-43px; \">";
@@ -27,7 +27,9 @@ function addCourse(testCourse) {
 
 function addClass(oCourse) {
 	var res = "";
-	res += "<li><div class=\"class-selector\" data-period=" + oCourse.period + " data-coursename=\""+oCourse.courseName + "\" data-classtime=\""+oCourse.classtime+"\" data-classroom=\""+oCourse.classroom+"\" data-teachers=\""+oCourse.teachers+ "\"><p>时间：";
+	res += "<li><div class=\"class-selector\" data-period=" + oCourse.period + " data-coursename=\"" + oCourse.courseName +
+		"\" data-classtime=\"" + oCourse.classtime + "\" data-classroom=\"" + oCourse.classroom + "\" data-teachers=\"" +
+		oCourse.teachers + "\"><p>时间：";
 	res += oCourse.classtime;
 	res += "</p><p>地点：";
 	res += oCourse.classroom;
@@ -38,32 +40,62 @@ function addClass(oCourse) {
 	return res;
 }
 
-function fillTable(obj){
+function fillTable(obj) {
+	// 
 	oDiv = obj.parentElement.parentElement;
+	oUl = oDiv.parentElement.parentElement;
+	aBtns = oUl.getElementsByClassName('mybtn-select');
 	period = oDiv.dataset.period.split(',');
 	name = oDiv.dataset.coursename;
 	time = oDiv.dataset.classtime;
 	room = oDiv.dataset.classroom;
 	teachers = oDiv.dataset.teachers;
 	var ot = document.getElementById("classtable");
-	var rown = 0;
-	var celln = 0;
-	for (var i = 0; i<ot.rows.length;i++){
-		for (var j = 0; j<ot.rows[0].cells.length; j++){
-			if(i == 2 || i == 5){
-				break;
-			}else if(ot.rows[i].cells[j].innerHTML.indexOf(name) != -1){
-				ot.rows[i].cells[j].innerHTML="";
+
+	if (obj.title == "pressed") {
+		for (var i = 0; i < period.length;) {
+			var rown = parseInt(period[i + 1] - 1);
+			var celln = parseInt(period[i]);
+			if (rown >= 2) rown++;
+			else if (rown >= 5) rown++;
+			ot.rows[rown].cells[celln].innerHTML = "";
+			i += 2;
+		}
+		obj.setAttribute("title", "released");
+	} else {
+		for (var i = 0; i < aBtns.length; i++) {
+			if (aBtns[i].title == "pressed") {
+				var tempperiod = aBtns[i].parentElement.parentElement.dataset.period.split(',');
+				aBtns[i].setAttribute("title", "released");
+				for (var i = 0; i < tempperiod.length;) {
+					var rown = parseInt(tempperiod[i + 1] - 1);
+					var celln = parseInt(tempperiod[i]);
+					if (rown >= 2) rown++;
+					else if (rown >= 5) rown++;
+					ot.rows[rown].cells[celln].innerHTML = "";
+					i += 2;
+				}
 			}
 		}
+
+		for (var i = 0; i < period.length;) {
+			var rown = parseInt(period[i + 1] - 1);
+			var celln = parseInt(period[i]);
+			if (rown >= 2) rown++;
+			else if (rown >= 5) rown++;
+			ot.rows[rown].cells[celln].innerHTML = "<p>" + name + "</p>" + "<p>" + teachers + "</p>" + "<p>" + time + "</p>" +
+				"<p>" + room + "</p>";
+			i += 2;
+		}
+		obj.setAttribute("title", "pressed");
 	}
-	for (var i = 0; i< period.length;){
-		rown = parseInt(period[i+1]-1);
-		celln = parseInt(period[i]);
-		if (rown >= 2) rown ++;
-		else if ( rown >= 5) rown ++;
-		ot.rows[rown].cells[celln].innerHTML = "<p>"+name+"</p>"+"<p>"+teachers+"</p>"+"<p>"+time+"</p>"+"<p>"+room+"</p>";
-		i+= 2;
-	}
-	
+}
+
+function removeLi(obj){
+	oDiv = obj.parentElement.parentElement;
+	oLi = oDiv.parentElement;
+	oUl = oLi.parentElement;
+	oDiv.innerHTML='';
+	startMove(oDiv,'height',0);
+	setTimeout("oUl.removeChild(oLi);",2000);
 }
